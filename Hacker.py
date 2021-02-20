cad66e54461f8748e84ece77fe97578053a0414b
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-20 19:59:10 +0530

    Create

---
+++ Hacker.py
@@ -0,0 +1,106 @@
+#python 3.7.1
+from tqdm import tqdm
+from time import sleep as sp
+import random
+name = str(input("enter user name : ".title()))
+app = str(input("enter app name: ".title()))
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbol = "[]{}()*;/,._-"
+all = lower + upper + number + symbol
+length = 6
+passwords= "".join(random.sample(all,length))
+
+for i in tqdm(range(100)):
+    time.sleep(0.1)
+def hack():
+  for h in f"\n\n\t\t\t\tHacking start ".upper():
+    print(h, end="", flush=True)
+    sp(.1)
+  story = f"""
+<html>
+ <head> 
+  <meta charset="UTF-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+  <title>{app} hacked</title> 
+  <link href="https://www.{app}.com/"> 
+ </head> 
+ <body> 
+  <div class="container"> 
+   <div class="equalizer"> 
+    <div class="info"> 
+     <div class="{app} info"></div> 
+    </div> 
+    <audio id="audio"></audio> 
+    <canvas id="equalizer_render"></canvas> 
+    <div class="progressBar"> 
+     <div class="seek"> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="audio-controls"> 
+    <div id="previousBtn" class="navigation-btn"> 
+     <div class="previous"></div> 
+     <div class="previous"></div> 
+    </div> 
+    <div id="playBtn" class="play-btn"> 
+     <div class="play"></div> 
+     <div class="pause"> 
+      <div></div> 
+      <div></div> 
+     </div> 
+    </div> 
+ </body>
+</html> 
+ _____________________
+|  ACCESS GRANHTED    |
+ —————————————————————
+ 
+<html>
+ <head> 
+  <meta charset="UTF-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+  <title>{name} info</title> 
+  <link href="https://www.{app}.com/{name}/"> 
+ </head> 
+ <body> 
+  <div class="container"> 
+   <div class="equalizer"> 
+    <div class="info"> 
+     <div class="{app} info"></div> 
+    </div> 
+    <audio id="audio"></audio> 
+    <canvas id="equalizer_render"></canvas> 
+    <div class="progressBar"> 
+     <div class="seek"> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="audio-controls"> 
+    <div id="previousBtn" class="navigation-btn"> 
+     <div class="previous"></div> 
+     <div class="previous"></div> 
+    </div>
+      <div></div> 
+     </div> 
+    </div> 
+ </body>
+</html> 
+ _____________________
+|  ACCESS GRANHTED    |
+ —————————————————————
+app name ={app}
+user_name={name}
+password ={passwords}
+
+   """.title()
+  for i in list(story):
+    print(i, end="", flush=True)
+    sp(.1)
+    
+hack()
+
+print("PLEASE GIVE ME A ⭐ FOR MY MOTIVATION")
